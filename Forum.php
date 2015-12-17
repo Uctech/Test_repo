31bdf8c5a1a4ac4dc271fcb0a1c9788632d2149f
calistus <icalistus@gmail.com>
2015-12-17 17:24:24 +0100

    I have added 2 new files.

---
+++ Forum.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+	<link rel='stylesheet' href='style/layout.css'>
+	<title>WAMP FORUM</title>
+</head>
+<body>
+	<center>
+	<img src="images/ForumBillBoard.JPG"></img>
+
+	<!--Code for the Menu-->
+	<div class= 'navbar'>
+	<div class='button'><a href='index.php'>Home</a></div>
+	<div class='button'><a href='#'>Forum</a></div>
+	<div class='button'><a href='#'>Gallery</a></div>
+	<div class='button'><a href='#'>Contact us</a></div>
+	<div class='button'><a href='#'>About</a></div>
+	</div>
+	
+	<table border="1"  width="680">
+		<tr>
+			<td><marquee>Welcome to our Forum</marquee></td>
+		</tr>
+	</table>
+	<form>
+		<table>
+			<tr><td>Subject:</td> <td>  <input type="text"></td>
+				</tr>
+			<tr><td>Message (required):</td> <td>  <textarea rows="5" cols="40" name="Message">
+					Enter your message here...
+				</textarea></td>
+
+
+		</table>
+	</form>
+	
+	</center>
+</body>
+
+</html>
+
+<!--php codes in here-->
+<?php
+
+?>
